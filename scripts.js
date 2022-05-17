9bde293e3aded53c96bd8bc71e8ae28535475892
LaggyWolf <aydencheong08@gmail.com>
2022-05-18 01:28:22 +0800

    wrote playRound func

--- scripts.js
+++ scripts.js
@@ -1,7 +1,35 @@
-const moves = ['Rock','Paper','Scissors'];
+const moves = ['rock','paper','scissors'];
+let playerMove = 0
 
 function computerPlay(){
     let moveNum = Math.floor(Math.random() * 3);
-    return moves[moveNum];
+    return moveNum;
 }
 
+function playRound(playerSelection,computerSelection){
+    for (let move in moves){
+        if (playerSelection.toLowerCase() == moves[move]){
+            playerMove = move;
+        };
+    };
+
+    if (playerMove == computerSelection){
+        return "It's a tie!";
+    }
+
+    let move_diff = playerMove - computerSelection;
+
+    if (move_diff**2 == 1){
+        if (move_diff < 0){
+            return `You lost, ${moves[computerSelection]} beats ${playerSelection.toLowerCase()}`;
+        }else{
+            return `You won, ${playerSelection.toLowerCase()} beats ${moves[computerSelection]}`;
+        }
+    }else if (move_diff**2 == 4){
+        if (move_diff < 0){
+            return `You won, ${playerSelection.toLowerCase()} beats ${moves[computerSelection]}`;
+        }else{
+            return `You lost, ${moves[computerSelection]} beats ${playerSelection.toLowerCase()}`;
+        }
+    }
+}
